17f3311dc718ff42eb495d7091d72310c8cb6891
Gabriel Alves <itsmealves@gmail.com>
2019-04-24 16:53:43 -0300

    compute all features

--- code/ComputeFullFeatures.m
+++ code/ComputeFullFeatures.m
@@ -1,4 +1,4 @@
-function features = ComputePositionColorFeatures(img)
+function features = ComputeFullFeatures(img)
 % Compute a feature vector of colors and positions for all pixels in the
 % image. For each pixel in the image we compute a feature vector
 % (r, g, b, x, y) where (r, g, b) is the color of the pixel and (x, y) is
@@ -14,7 +14,7 @@ function features = ComputePositionColorFeatures(img)
 
     height = size(img, 1);
     width = size(img, 2);
-    features = zeros(height, width, 5);
+    features = zeros(height, width, 6);
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %                                                                         %
     %                              YOUR CODE HERE                             %
@@ -22,7 +22,6 @@ function features = ComputePositionColorFeatures(img)
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     [x y] = meshgrid(1:width, 1:height);
-    features(:,:,1:3) = ComputeColorFeatures(img);
-    features(:,:,4) = x;
-    features(:,:,5) = y;
+    features(:,:,1:5) = ComputePositionColorFeatures(img);
+    features(:,:,6) = ComputeFeatures(img);
 end
